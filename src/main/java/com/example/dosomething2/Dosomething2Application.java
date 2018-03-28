e6dedc380defa525fd58cfdbfa5dfcb38c0f0fa2
Eden Bereda <edukenat@gmail.com>
2018-03-28 15:35:34 -0400

    Initial commit

---
+++ src/main/java/com/example/dosomething2/Dosomething2Application.java
@@ -0,0 +1,12 @@
+package com.example.dosomething2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dosomething2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dosomething2Application.class, args);
+	}
+}
